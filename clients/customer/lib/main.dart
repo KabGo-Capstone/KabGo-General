de81bc85f6bc5eec83bcb2f023d6767c390a4b92
khangduy017 <khangduy017@gmail.com>
2024-02-26 21:10:04 +0700

    clean dependency

--- clients/customer/lib/main.dart
+++ clients/customer/lib/main.dart
@@ -9,7 +9,6 @@ void main() async {
   // WidgetsFlutterBinding.ensureInitialized();
   // await Firebase.initializeApp();
   // await Notifications().initNotifications();
-  // await Permission.location.request();
   runApp(
     const ProviderScope(
       child: App(),
