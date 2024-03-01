a21d8900925554bed6246e8c0b34c130951582cc
NG-MinG <wgmin.it@outlook.com>
2024-03-01 14:16:20 +0700

    update deploy script

--- servers/admin/common/services/demand.service.ts
+++ servers/admin/common/services/demand.service.ts
@@ -1,56 +1,56 @@
-// import * as grpc from '@grpc/grpc-js'
-// import { CustomerInfomationsClient } from './../../../grpc/proto_pb/demand/demand_grpc_pb'
-// import {
-//     CustomerId,
-//     CustomerInfomation,
-// } from '../../../grpc/proto_pb/demand/demand_pb'
-// import Logger from '../utils/logger'
-// import chalk from 'chalk'
+import * as grpc from '@grpc/grpc-js'
+import { CustomerInfomationsClient } from './../../../grpc/proto_pb/demand/demand_grpc_pb'
+import {
+    CustomerId,
+    CustomerInfomation,
+} from '../../../grpc/proto_pb/demand/demand_pb'
+import Logger from '../utils/logger'
+import chalk from 'chalk'
 
-// class DemandStub {
-//     private static instance: DemandStub
-//     private demandsStub: CustomerInfomationsClient
+class DemandStub {
+    private static instance: DemandStub
+    private demandsStub: CustomerInfomationsClient
 
-//     private constructor() {
-//         this.demandsStub = new CustomerInfomationsClient(
-//             `${process.env.DEMAND_GRPC_CLIENT ?? '127.0.0.1'}:50051`,
-//             grpc.credentials.createInsecure()
-//         )
+    private constructor() {
+        this.demandsStub = new CustomerInfomationsClient(
+            `${process.env.DEMAND_GRPC_CLIENT ?? '127.0.0.1'}:50051`,
+            grpc.credentials.createInsecure()
+        )
 
-//         const deadline = new Date()
-//         deadline.setSeconds(deadline.getSeconds() + 20)
-//         this.demandsStub.waitForReady(deadline, (error?: Error) => {
-//             if (error) {
-//                 Logger.error(`Supply stub connect error: ${error.message}`)
-//             } else {
-//                 Logger.info(
-//                     chalk.green('Connect to demand grpc server successfully')
-//                 )
-//                 this.test()
-//             }
-//         })
-//     }
+        const deadline = new Date()
+        deadline.setSeconds(deadline.getSeconds() + 20)
+        this.demandsStub.waitForReady(deadline, (error?: Error) => {
+            if (error) {
+                Logger.error(`Supply stub connect error: ${error.message}`)
+            } else {
+                Logger.info(
+                    chalk.green('Connect to demand grpc server successfully')
+                )
+                this.test()
+            }
+        })
+    }
 
-//     public static connect(): DemandStub {
-//         return DemandStub.instance ?? (DemandStub.instance = new DemandStub())
-//     }
+    public static connect(): DemandStub {
+        return DemandStub.instance ?? (DemandStub.instance = new DemandStub())
+    }
 
-//     public test() {
-//         this.find('customer-1002')
-//     }
+    public test() {
+        this.find('customer-1002')
+    }
 
-//     public find(driverId: string): void {
-//         const message = new CustomerId()
-//         message.setId(driverId)
+    public find(driverId: string): void {
+        const message = new CustomerId()
+        message.setId(driverId)
 
-//         this.demandsStub.find(message, (err: any, data: CustomerInfomation) => {
-//             if (err) {
-//                 Logger.error(err)
-//             } else {
-//                 console.log(data.toObject())
-//             }
-//         })
-//     }
-// }
+        this.demandsStub.find(message, (err: any, data: CustomerInfomation) => {
+            if (err) {
+                Logger.error(err)
+            } else {
+                console.log(data.toObject())
+            }
+        })
+    }
+}
 
-// export default DemandStub
+export default DemandStub
