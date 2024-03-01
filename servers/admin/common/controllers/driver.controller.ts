bcb157ce1c4f829f366e626973b8f797e0484098
GiaHuy <giahuy200202@gmail.com>
2024-03-01 10:33:51 +0700

    api for  get, verify, delete, create driver with dummy data

---
+++ servers/admin/common/controllers/driver.controller.ts
@@ -0,0 +1,67 @@
+import mongoose from 'mongoose'
+import { NextFunction, Request, Response, Router } from 'express'
+import IController from '../interfaces/controller'
+import UserModel from '../models/user.example.model'
+import catchAsync from '../utils/catch.error'
+import DTOValidation from '../middlewares/validation.middleware'
+import UserDTO from '../dtos/user.example.dto'
+import Jwt, { JsonWebToken } from '../utils/jwt'
+// import GMailer from '../services/mailer.builder'
+import cacheMiddleware from '../middlewares/cache.middleware'
+import redis from '../services/redis'
+import MulterCloudinaryUploader from '../multer'
+import Logger from '../utils/logger'
+import * as DummyData from '../dummy_data/dummy_data'
+
+let data = [...DummyData.supplies];
+
+class DriverController implements IController {
+    readonly path: string = '/driver'
+    readonly router: Router = Router()
+
+    constructor() {
+        this.router.get(
+            '/',
+            catchAsync(this.getDriver)
+        )
+        this.router.post(
+            '/verify/:id',
+            catchAsync(this.verifyDriver)
+        )
+        this.router.post(
+            '/delete/:id',
+            catchAsync(this.deleteDriver)
+        )
+        this.router.post(
+            '/create',
+            catchAsync(this.createDriver)
+        )
+    }
+
+    private async getDriver(req: Request, res: Response, next: NextFunction) {
+        return res.status(200).json({ data: data })
+    }
+
+    private async verifyDriver(req: Request, res: Response, next: NextFunction) {
+        const index: number = data.findIndex(el => el.id === req.params.id);
+        data[index].verified = !data[index].verified;
+        return res.status(200).json({ data: data });
+    }
+
+    private async deleteDriver(req: Request, res: Response, next: NextFunction) {
+        data = data.filter(el => el.id !== req.params.id);
+        return res.status(200).json({ data: data });
+    }
+
+    private async createDriver(req: Request, res: Response, next: NextFunction) {
+        const createData = {
+            id: String(data.length + 1),
+            ...req.body
+        }
+        data.push(createData);
+        return res.status(200).json({ data: data});
+    }
+
+}
+
+export default new DriverController()
--- servers/admin/common/services/supply.service.ts
+++ servers/admin/common/services/supply.service.ts
@@ -1,56 +1,56 @@
-import * as grpc from '@grpc/grpc-js'
-import { DriverInfomationsClient } from './../../../grpc/proto_pb/supply/supply_grpc_pb'
-import {
-    DriverId,
-    DriverInfomation,
-} from '../../../grpc/proto_pb/supply/supply_pb'
-import Logger from '../utils/logger'
-import chalk from 'chalk'
+// import * as grpc from '@grpc/grpc-js'
+// import { DriverInfomationsClient } from './../../../grpc/proto_pb/supply/supply_grpc_pb'
+// import {
+//     DriverId,
+//     DriverInfomation,
+// } from '../../../grpc/proto_pb/supply/supply_pb'
+// import Logger from '../utils/logger'
+// import chalk from 'chalk'
 
-class SupplyStub {
-    private static instance: SupplyStub
-    private supplysStub: DriverInfomationsClient
+// class SupplyStub {
+//     private static instance: SupplyStub
+//     private supplysStub: DriverInfomationsClient
 
-    private constructor() {
-        this.supplysStub = new DriverInfomationsClient(
-            `${process.env.SUPPLY_GRPC_CLIENT ?? '127.0.0.1'}:50052`,
-            grpc.credentials.createInsecure()
-        )
+//     private constructor() {
+//         this.supplysStub = new DriverInfomationsClient(
+//             `${process.env.SUPPLY_GRPC_CLIENT ?? '127.0.0.1'}:50052`,
+//             grpc.credentials.createInsecure()
+//         )
 
-        const deadline = new Date()
-        deadline.setSeconds(deadline.getSeconds() + 20)
-        this.supplysStub.waitForReady(deadline, (error?: Error) => {
-            if (error) {
-                console.log(`Client connect error: ${error.message}`)
-            } else {
-                Logger.info(
-                    chalk.green('Connect to supply grpc server successfully')
-                )
-                this.test()
-            }
-        })
-    }
+//         const deadline = new Date()
+//         deadline.setSeconds(deadline.getSeconds() + 20)
+//         this.supplysStub.waitForReady(deadline, (error?: Error) => {
+//             if (error) {
+//                 console.log(`Client connect error: ${error.message}`)
+//             } else {
+//                 Logger.info(
+//                     chalk.green('Connect to supply grpc server successfully')
+//                 )
+//                 this.test()
+//             }
+//         })
+//     }
 
-    public static connect(): SupplyStub {
-        return SupplyStub.instance ?? (SupplyStub.instance = new SupplyStub())
-    }
+//     public static connect(): SupplyStub {
+//         return SupplyStub.instance ?? (SupplyStub.instance = new SupplyStub())
+//     }
 
-    public test() {
-        this.find('driver-1002')
-    }
+//     public test() {
+//         this.find('driver-1002')
+//     }
 
-    public find(driverId: string): void {
-        const message = new DriverId()
-        message.setId(driverId)
+//     public find(driverId: string): void {
+//         const message = new DriverId()
+//         message.setId(driverId)
 
-        this.supplysStub.find(message, (err: any, data: DriverInfomation) => {
-            if (err) {
-                Logger.error(err)
-            } else {
-                console.log(data.toObject())
-            }
-        })
-    }
-}
+//         this.supplysStub.find(message, (err: any, data: DriverInfomation) => {
+//             if (err) {
+//                 Logger.error(err)
+//             } else {
+//                 console.log(data.toObject())
+//             }
+//         })
+//     }
+// }
 
-export default SupplyStub
+// export default SupplyStub
