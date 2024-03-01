79732943214ba72e8592f69cd143a023256f5d57
NG-MinG <wgmin.it@outlook.com>
2024-03-01 22:59:07 +0700

    trigger deployment

--- servers/grpc/build.sh
+++ servers/grpc/build.sh
@@ -28,7 +28,7 @@ npx grpc_tools_node_protoc \
     --ts_proto_out="${SOURCE_DIR}" \
     --ts_proto_opt=outputServices=grpc-js \
     --ts_proto_opt=esModuleInterop=true \
-    -I="${PROTO_DIR}" "${PROTO_DIR}/*.proto"
+    -I "${PROTO_DIR}" "${PROTO_DIR}/*.proto"
 
 # npx grpc_tools_node_protoc \
 #     --grpc_out="grpc_js:${SOURCE_DIR}" \
