17dffc7b54224eaa5eac2954787e8c9934a516ef
PhongTran1804 <trannguyenphongg@gmail.com>
2024-03-11 14:28:11 +0700

    update UI admin

--- clients/admin/src/components/Details/step2content.tsx
+++ clients/admin/src/components/Details/step2content.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Typography, Image, theme } from "antd";
+import NoImg from '../../assets/images/no_img.jpg';
+import GiayPhepLaiXe from '../../assets/images/giay_phep_lai_xe.jpg';
+import CCCD from '../../assets/images/identity card.jpg';
+import CCCD2 from '../../assets/images/motocycle.png';
 
 const Step2Content: React.FC<any> = ({ record }) => {
   const {
@@ -7,38 +11,49 @@ const Step2Content: React.FC<any> = ({ record }) => {
   } = theme.useToken();
 
   return (
-    <div className="flex justify-between space-x-4" style={{ padding: 24, background: colorBgContainer, borderRadius: borderRadiusLG }}>
-      <div className="flex-1 space-y-4">
-        {renderCard("CCCD/CMND/Hộ chiếu", "Mặt trước:", record?.identityImgFrontsight)}
-        {renderCard("CCCD/CMND/Hộ chiếu", "Mặt sau:", record?.identityImgBacksight)}
-      </div>
+    // <div className="flex justify-between space-x-4" style={{ padding: 24, background: colorBgContainer, borderRadius: borderRadiusLG }}>
+    //   <div className="flex-1 space-y-4">
+    //     {renderCard("CCCD/CMND/Hộ chiếu", "Mặt trước:", CCCD)}
+    //     {renderCard("CCCD/CMND/Hộ chiếu", "Mặt sau:", CCCD2)}
+    //   </div>
 
-      {/* Vertical Line */}
-      <div className="!border-l !border-gray-300 !h-full"></div>
+    //   {/* Vertical Line */}
+    //   {/* <div className="!border-l !border-gray-300 !h-full"></div> */}
 
-      <div className="flex-1 flex flex-col items-center">
-        {renderCard("Giấy đăng ký xe", "Mặt trước:", record?.vehicleRegistrationFrontsight)}
-        {renderCard("Giấy đăng ký xe", "Mặt sau:", record?.vehicleRegistrationBacksight)}
-      </div>
+    //   <div className="flex-1 flex flex-col items-center">
+    //     {renderCard("Giấy đăng ký xe", "Mặt trước:", GiayPhepLaiXe)}
+    //     {renderCard("Giấy đăng ký xe", "Mặt sau:", NoImg)}
+    //   </div>
+    // </div>
+    <div
+      className="grid grid-cols-2 gap-4"
+      style={{ padding: 24, background: colorBgContainer, borderRadius: borderRadiusLG }}
+    >
+      {renderCard("CCCD/CMND/Hộ chiếu", "Mặt trước:", record?.identityImgFrontsight)}
+      {renderCard("CCCD/CMND/Hộ chiếu", "Mặt sau:", record?.identityImgBacksight)}
+      {renderCard("Giấy đăng ký xe", "Mặt trước:", record?.vehicleRegistrationFrontsight)}
+      {renderCard("Giấy đăng ký xe", "Mặt sau:", record?.vehicleRegistrationBacksight)}
     </div>
+
   );
 };
 
-const renderCard = (title: string, subTitle: string, imageSource:any) => (
-  <div className="flex flex-col items-center !m-5 !h-35">
+const renderCard = (title: string, subTitle: string, imageSource: any) => (
+  <div className="flex grow flex-col items-center !m-5 !h-35">
     <Typography.Text className="!text-2xl font-bold !mb-2">{title}</Typography.Text>
     <Typography.Text className="!text-lg !mb-2">{subTitle}</Typography.Text>
     <Image
-        width={300}
-        src={imageSource}
-        placeholder={
-          <Image
-            preview={false}
-            src={imageSource    }
-            width={200}
-          />
-        }
-      />
+      width={300}
+      src={imageSource || NoImg}
+      // className="!bg-cover"
+      placeholder={
+        <Image
+          preview={false}
+          src={imageSource || NoImg}
+        // width={500}
+        />
+      }
+    />
   </div>
 );
 
--- clients/admin/src/components/Details/step4content.tsx
+++ clients/admin/src/components/Details/step4content.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Typography, Image, theme } from "antd";
 import GiayPhepLaiXe from '../../assets/images/giay_phep_lai_xe.jpg';
 // import BaoHiemXe from '../../assets/images/bao_hiem_Xe.jpg';
+import NoImg from '../../assets/images/no_img.jpg';
 
 const Step4Content: React.FC<any> = ({ record }) => {
   const {
@@ -9,31 +10,47 @@ const Step4Content: React.FC<any> = ({ record }) => {
   } = theme.useToken();
 
   return (
-    <div className="flex justify-between space-x-4" style={{ padding: 24, background: colorBgContainer, borderRadius: borderRadiusLG }}>
-      <div className="flex-1 flex flex-col items-center space-y-4">
-        <Typography.Text className="!text-2xl font-bold !mb-2">Giấy phép lái xe</Typography.Text>
-        {renderCard("Mặt trước", GiayPhepLaiXe)}
-        {renderCard("Mặt sau", GiayPhepLaiXe)}
-      </div>
+    // <div className="flex justify-between space-x-4" style={{ padding: 24, background: colorBgContainer, borderRadius: borderRadiusLG }}>
+    //   <div className="flex-1 flex flex-col items-center space-y-4">
+    //     <Typography.Text className="!text-2xl font-bold !mb-2">Giấy phép lái xe</Typography.Text>
+    //     {renderCard("Mặt trước", GiayPhepLaiXe)}
+    //     {renderCard("Mặt sau", GiayPhepLaiXe)}
+    //   </div>
 
-      {/* Second Column for Vehicle Image */}
-      <div className="flex-1 flex flex-col items-center space-y-4">
-        <Typography.Text className="!text-2xl font-bold !mb-2">Bảo hiểm xe</Typography.Text>
-        {renderCard("Mặt trước", record?.vehicleInsuranceFrontsight)}
-        {renderCard("Mặt sau", record?.vehicleInsuranceBacksight)}
-      </div>
+    //   {/* Second Column for Vehicle Image */}
+    //   <div className="flex-1 flex flex-col items-center space-y-4">
+    //     <Typography.Text className="!text-2xl font-bold !mb-2">Bảo hiểm xe</Typography.Text>
+    //     {renderCard("Mặt trước", record?.vehicleInsuranceFrontsight)}
+    //     {renderCard("Mặt sau", record?.vehicleInsuranceBacksight)}
+    //   </div>
+    // </div>
+    <div
+      className="grid grid-cols-2 gap-4"
+      style={{ padding: 24, background: colorBgContainer, borderRadius: borderRadiusLG }}
+    >
+      <Typography.Text className="!text-2xl flex justify-center font-bold !mb-2">Giấy phép lái xe</Typography.Text>
+      <Typography.Text className="!text-2xl flex justify-center font-bold !mb-2">Bảo hiểm xe</Typography.Text>
+      {renderCard("Mặt trước", NoImg)}
+      {renderCard("Mặt trước", record?.vehicleInsuranceFrontsight)}
+      {renderCard("Mặt sau", NoImg)}
+      {renderCard("Mặt sau", record?.vehicleInsuranceBacksight)}
     </div>
   );
 };
 
 const renderCard = (title: string, imageSource: any) => (
   <div className="flex flex-col items-center">
-    {/* <Typography.Text className="!text-2xl font-bold !mb-2">{title}</Typography.Text> */}
     <Typography.Text className="!text-lg !mb-2">{title}</Typography.Text>
     <Image
       width={300}
-      src={imageSource}
-      placeholder={<Image preview={false} src={imageSource} width={200} />}
+      src={imageSource || NoImg} 
+      placeholder={
+        <Image
+          preview={false}
+          src={imageSource || NoImg}
+          width={200}
+        />
+      }
     />
   </div>
 );
