69492ef465bfac463d8ce36a6b13f078c6ae4eae
Bui Quang Thanh <buiquangthanh1709@gmail.com>
2024-03-26 21:53:49 +0700

    [Feat] Implement UI login and login with phone

---
+++ clients/customer/lib/screens/authen/login/function/get_data_mobile_phone.dart
@@ -0,0 +1,16 @@
+List<Map<String, dynamic>> getDataMobilePhone() {
+  return [
+    {'icon': 'lib/assets/images/vietnam_flag.png', 'value': '+84', 'title': 'Việt Nam'},
+    {'icon': 'lib/assets/images/angola_logo.png', 'value': '+376', 'title': 'Angola'},
+    {'icon': 'lib/assets/images/afghanistan_logo.png', 'value': '+93', 'title': 'Afghanistan'},
+    {'icon': 'lib/assets/images/albania_logo.png', 'value': '+355', 'title': 'Albania'},
+    {'icon': 'lib/assets/images/algeria_logo.png', 'value': '+213', 'title': 'Algeria'},
+    {'icon': 'lib/assets/images/andorra_logo.png', 'value': '+376', 'title': 'Andorra'},
+    {'icon': 'lib/assets/images/angola_logo.png', 'value': '+244', 'title': 'Angola'},
+    {'icon': 'lib/assets/images/antigua_and_barbuda_logo.png', 'value': '+1-268', 'title': 'Antigua and Barbuda'},
+    {'icon': 'lib/assets/images/argentina_logo.png', 'value': '+54', 'title': 'Argentina'},
+    {'icon': 'lib/assets/images/armenia_logo.png', 'value': '+374', 'title': 'Armenia'},
+    {'icon': 'lib/assets/images/australia_logo.png', 'value': '+61', 'title': 'Australia'},
+    {'icon': 'lib/assets/images/austria_logo.png', 'value': '+43', 'title': 'Austria'},
+  ];
+}
