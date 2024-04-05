fbf8ef20acb1b68c66fb5a6eb7ef7cd1531ee5e7
Khoa Nguyen <nguyenthoaidangkhoa@gmail.com>
2024-04-05 15:19:52 +0700

    [feat] demand service with kafkaJS

---
+++ PoC/demand/common/controllers/index.ts
@@ -0,0 +1,3 @@
+export { default as UserController } from './user.example.controller'
+export { default as MessageController } from './message.controller'
+
---
+++ PoC/demand/common/events/coordinator.event.ts
@@ -0,0 +1,34 @@
+import { Server, Socket } from 'socket.io'
+import IEvent from '../interfaces/event'
+import clientSocket from 'common/socket.client'
+import { sendMessage } from 'common/utils/kafka'
+
+class CoordinatorEvent implements IEvent {
+    public readonly event: string = 'coordinator'
+    public readonly listener: (
+        io: Server,
+        socket: Socket,
+        ...args: any[]
+    ) => void
+
+    constructor() {
+        this.listener = this.onCoordinator
+    }
+
+    private async onCoordinator(io: Server, socket: Socket, user: any) {
+        io.customer_sockets = {
+            ...io.customer_sockets,
+            [user.id]: {
+                ...user,
+                socket: socket,
+            },
+        }
+
+        console.log('user: ', user);
+
+        // clientSocket.client().emit('coordinator', user) // change it to message brokers
+        sendMessage('demand-booking', JSON.stringify(user)); // send user booking information to kafka topic 'demand-booking',
+    }
+}
+
+export default new CoordinatorEvent()
